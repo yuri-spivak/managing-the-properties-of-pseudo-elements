a6dcca81da126f11bb13e0117c04dba47e854abe
yuri-spivak <yurka_mail@ukr.net>
2017-08-05 14:15:04 +0300

    Add files via upload

---
+++ plugin/jquery-plugin.js
@@ -0,0 +1,121 @@
+(function () {
+    $.pseudoElements = {length: 0};
+
+    var setPseudoElement = function (parameters) {
+        if (typeof parameters.argument === 'object' || (parameters.argument !== undefined && parameters.property !== undefined)) {
+            for (var element of parameters.elements.get()) {
+                if (!element.pseudoElements) element.pseudoElements = { styleSheet: null, before: { index: null, properties: null }, after: { index: null, properties: null }, id: null };
+
+                var selector = (function() {
+                    if (element.pseudoElements.id !== null) {
+                        if (Number(element.getAttribute('data-pe--id')) !== element.pseudoElements.id) element.setAttribute('data-pe--id', element.pseudoElements.id);
+                        return '[data-pe--id="' + element.pseudoElements.id + '"]::' + parameters.pseudoElement;
+                    } else {
+                        var id = $.pseudoElements.length;
+                        $.pseudoElements.length++
+
+                        element.pseudoElements.id = id;
+                        element.setAttribute('data-pe--id', id);
+
+                        return '[data-pe--id="' + id + '"]::' + parameters.pseudoElement;
+                    };
+                })();
+
+                if (!element.pseudoElements.styleSheet) {
+                    if (document.styleSheets[0]) {
+                        element.pseudoElements.styleSheet = document.styleSheets[0];
+                    } else {
+                        var styleSheet = document.createElement('style');
+
+                        document.head.appendChild(styleSheet);
+                        element.pseudoElements.styleSheet = styleSheet.sheet;
+                    };
+                };
+
+                if (element.pseudoElements[parameters.pseudoElement].properties && element.pseudoElements[parameters.pseudoElement].index) {
+                    element.pseudoElements.styleSheet.deleteRule(element.pseudoElements[parameters.pseudoElement].index);
+                };
+
+                if (typeof parameters.argument === 'object') {
+                    parameters.argument = $.extend({}, parameters.argument);
+
+                    if (!element.pseudoElements[parameters.pseudoElement].properties && !element.pseudoElements[parameters.pseudoElement].index) {
+                        var newIndex = element.pseudoElements.styleSheet.rules.length || element.pseudoElements.styleSheet.cssRules.length || element.pseudoElements.styleSheet.length;
+
+                        element.pseudoElements[parameters.pseudoElement].index = newIndex;
+                        element.pseudoElements[parameters.pseudoElement].properties = parameters.argument;
+                    };
+
+                    var properties = '';
+
+                    for (var property in parameters.argument) {
+                        if (typeof parameters.argument[property] === 'function')
+                            element.pseudoElements[parameters.pseudoElement].properties[property] = parameters.argument[property]();
+                        else
+                            element.pseudoElements[parameters.pseudoElement].properties[property] = parameters.argument[property];
+                    };
+
+                    for (var property in element.pseudoElements[parameters.pseudoElement].properties) {
+                        properties += property + ': ' + element.pseudoElements[parameters.pseudoElement].properties[property] + ' !important; ';
+                    };
+
+                    element.pseudoElements.styleSheet.addRule(selector, properties, element.pseudoElements[parameters.pseudoElement].index);
+                } else if (parameters.argument !== undefined && parameters.property !== undefined) {
+                    if (!element.pseudoElements[parameters.pseudoElement].properties && !element.pseudoElements[parameters.pseudoElement].index) {
+                        var newIndex = element.pseudoElements.styleSheet.rules.length || element.pseudoElements.styleSheet.cssRules.length || element.pseudoElements.styleSheet.length;
+
+                        element.pseudoElements[parameters.pseudoElement].index = newIndex;
+                        element.pseudoElements[parameters.pseudoElement].properties = {};
+                    };
+
+                    if (typeof parameters.property === 'function')
+                        element.pseudoElements[parameters.pseudoElement].properties[parameters.argument] = parameters.property();
+                    else
+                        element.pseudoElements[parameters.pseudoElement].properties[parameters.argument] = parameters.property;
+
+                    var properties = '';
+
+                    for (var property in element.pseudoElements[parameters.pseudoElement].properties) {
+                        properties += property + ': ' + element.pseudoElements[parameters.pseudoElement].properties[property] + ' !important; ';
+                    };
+
+                    element.pseudoElements.styleSheet.addRule(selector, properties, element.pseudoElements[parameters.pseudoElement].index);
+                };
+            };
+
+            return $(parameters.elements);
+        } else if (parameters.argument !== undefined && parameters.property === undefined) {
+            var element = $(parameters.elements).get(0);
+
+            var windowStyle = window.getComputedStyle(
+	            element, '::' + parameters.pseudoElement
+            ).getPropertyValue(parameters.argument);
+
+            if (element.pseudoElements) {
+                return $(parameters.elements).get(0).pseudoElements[parameters.pseudoElement].properties[parameters.argument] || windowStyle;
+            } else {
+                return windowStyle || null;
+            };
+        } else {
+            console.error('Invalid values!');
+            return false;
+        };
+    };
+
+    $.fn.cssBefore = function (argument, property) {
+        return setPseudoElement({
+            elements: this,
+            pseudoElement: 'before',
+            argument: argument,
+            property: property
+        });
+    };
+    $.fn.cssAfter = function (argument, property) {
+        return setPseudoElement({
+            elements: this,
+            pseudoElement: 'after',
+            argument: argument,
+            property: property
+        });
+    };
+})();
---
+++ plugin/plugin.js
@@ -0,0 +1,129 @@
+(function () {
+    document.pseudoElements = { length: 0 };
+
+    var setPseudoElement = function (parameters) {
+        if (typeof parameters.argument === 'object' || (parameters.argument !== undefined && parameters.property !== undefined)) {
+            if (!parameters.element.pseudoElements) parameters.element.pseudoElements = { styleSheet: null, before: { index: null, properties: null }, after: { index: null, properties: null }, id: null };
+
+            var selector = (function () {
+                if (parameters.element.pseudoElements.id !== null) {
+                    if (Number(parameters.element.getAttribute('data-pe--id')) !== parameters.element.pseudoElements.id) parameters.element.setAttribute('data-pe--id', parameters.element.pseudoElements.id);
+                    return '[data-pe--id="' + parameters.element.pseudoElements.id + '"]::' + parameters.pseudoElement;
+                } else {
+                    var id = document.pseudoElements.length;
+                    document.pseudoElements.length++
+
+                    parameters.element.pseudoElements.id = id;
+                    parameters.element.setAttribute('data-pe--id', id);
+
+                    return '[data-pe--id="' + id + '"]::' + parameters.pseudoElement;
+                };
+            })();
+
+            if (!parameters.element.pseudoElements.styleSheet) {
+                if (document.styleSheets[0]) {
+                    parameters.element.pseudoElements.styleSheet = document.styleSheets[0];
+                } else {
+                    var styleSheet = document.createElement('style');
+
+                    document.head.appendChild(styleSheet);
+                    parameters.element.pseudoElements.styleSheet = styleSheet.sheet;
+                };
+            };
+
+            if (parameters.element.pseudoElements[parameters.pseudoElement].properties && parameters.element.pseudoElements[parameters.pseudoElement].index) {
+                parameters.element.pseudoElements.styleSheet.deleteRule(parameters.element.pseudoElements[parameters.pseudoElement].index);
+            };
+
+            if (typeof parameters.argument === 'object') {
+                parameters.argument = (function() {
+                    var cloneObject = typeof parameters.argument.pop === 'function' ? [] : {};
+
+                    for (var property in parameters.argument) {
+                        cloneObject[property] = parameters.argument[property];
+                    };
+
+                    return cloneObject;
+                })();
+
+                if (!parameters.element.pseudoElements[parameters.pseudoElement].properties && !parameters.element.pseudoElements[parameters.pseudoElement].index) {
+                    var newIndex = parameters.element.pseudoElements.styleSheet.rules.length || parameters.element.pseudoElements.styleSheet.cssRules.length || parameters.element.pseudoElements.styleSheet.length;
+
+                    parameters.element.pseudoElements[parameters.pseudoElement].index = newIndex;
+                    parameters.element.pseudoElements[parameters.pseudoElement].properties = parameters.argument;
+                };
+
+                var properties = '';
+
+                for (var property in parameters.argument) {
+                    if (typeof parameters.argument[property] === 'function')
+                        parameters.element.pseudoElements[parameters.pseudoElement].properties[property] = parameters.argument[property]();
+                    else
+                        parameters.element.pseudoElements[parameters.pseudoElement].properties[property] = parameters.argument[property];
+                };
+
+                for (var property in parameters.element.pseudoElements[parameters.pseudoElement].properties) {
+                    properties += property + ': ' + parameters.element.pseudoElements[parameters.pseudoElement].properties[property] + ' !important; ';
+                };
+
+                parameters.element.pseudoElements.styleSheet.addRule(selector, properties, parameters.element.pseudoElements[parameters.pseudoElement].index);
+            } else if (parameters.argument !== undefined && parameters.property !== undefined) {
+                if (!parameters.element.pseudoElements[parameters.pseudoElement].properties && !parameters.element.pseudoElements[parameters.pseudoElement].index) {
+                    var newIndex = parameters.element.pseudoElements.styleSheet.rules.length || parameters.element.pseudoElements.styleSheet.cssRules.length || parameters.element.pseudoElements.styleSheet.length;
+
+                    parameters.element.pseudoElements[parameters.pseudoElement].index = newIndex;
+                    parameters.element.pseudoElements[parameters.pseudoElement].properties = {};
+                };
+
+                if (typeof parameters.property === 'function')
+                    parameters.element.pseudoElements[parameters.pseudoElement].properties[parameters.argument] = parameters.property();
+                else
+                    parameters.element.pseudoElements[parameters.pseudoElement].properties[parameters.argument] = parameters.property;
+
+                var properties = '';
+
+                for (var property in parameters.element.pseudoElements[parameters.pseudoElement].properties) {
+                    properties += property + ': ' + parameters.element.pseudoElements[parameters.pseudoElement].properties[property] + ' !important; ';
+                };
+
+                parameters.element.pseudoElements.styleSheet.addRule(selector, properties, parameters.element.pseudoElements[parameters.pseudoElement].index);
+            };
+        } else if (parameters.argument !== undefined && parameters.property === undefined) {
+            var windowStyle = window.getComputedStyle(
+                parameters.element, '::' + parameters.pseudoElement
+            ).getPropertyValue(parameters.argument);
+
+            if (parameters.element.pseudoElements) {
+                return parameters.element.pseudoElements[parameters.pseudoElement].properties[parameters.argument] || windowStyle;
+            } else {
+                return windowStyle || null;
+            };
+        } else {
+            console.error('Invalid values!');
+            return false;
+        };
+    };
+
+    Object.defineProperty(Element.prototype, 'styleBefore', {
+        enumerable: false,
+        value: function(argument, property) {
+            return setPseudoElement({
+                element: this,
+                pseudoElement: 'before',
+                argument: argument,
+                property: property
+            });
+        }
+    });
+    Object.defineProperty(Element.prototype, 'styleAfter', {
+        enumerable: false,
+        value: function(argument, property) {
+            return setPseudoElement({
+                element: this,
+                pseudoElement: 'after',
+                argument: argument,
+                property: property
+            });
+        }
+    });
+})();
